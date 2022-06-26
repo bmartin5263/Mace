0f87f44c581b246262441b61f57370f554e2c045
Brandon <bsm2112@yahoo.com>
2022-06-25 20:53:10 -0500

    ditch the modules im not using, update android project

--- cmake/modules/android/Compiler/XL-CXX-DetermineCompiler.cmake
+++
@@ -1,4 +0,0 @@
-
-set(_compiler_id_pp_test "defined(__IBMCPP__) && !defined(__COMPILER_VER__) && __IBMCPP__ >= 800")
-
-include("IBMCPP-CXX-DetermineVersionInternal.cmake")
--- cmake/modules/android/Compiler/zOS-CXX-DetermineCompiler.cmake
+++
@@ -1,4 +0,0 @@
-
-set(_compiler_id_pp_test "defined(__IBMCPP__) && defined(__COMPILER_VER__)")
-
-include("IBMCPP-CXX-DetermineVersionInternal.cmake")
